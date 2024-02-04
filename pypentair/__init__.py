493fbe9ebb27b8274befb3a5657ba53d3c15461c
John Gemkow <jgemkow94@gmail.com>
2024-02-04 15:33:16 -0600

    Incrmented version to 0.0.1

--- pypentair/__init__.py
+++ pypentair/__init__.py
@@ -3,4 +3,4 @@ from .exceptions import PentairApiException, PentairAuthenticationError
 from .pentair import Pentair
 
 __all__ = ["Pentair", "PentairApiException", "PentairAuthenticationError"]
-__version__ = "0.0.0"
+__version__ = "0.0.1"
