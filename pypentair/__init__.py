e43e4ca41a0d9058feb916603423af26d8e6abd3
John Gemkow <jgemkow94@gmail.com>
2024-02-06 16:21:10 -0600

    Fix for imports of other Pentair classes

--- pypentair/__init__.py
+++ pypentair/__init__.py
@@ -1,6 +1,7 @@
 """pypentair module."""
 from .exceptions import PentairApiException, PentairAuthenticationError
-from .pentair import Pentair
+from .pentair import Pentair, PentairDevice, PentairIF3Pump, PentairIF3PumpProgram, PentairSaltLevelSensor, PentairSumpPumpBatteryBackup
 
-__all__ = ["Pentair", "PentairApiException", "PentairAuthenticationError"]
+__all__ = ["Pentair", "PentairApiException", "PentairAuthenticationError", "PentairDevice", "PentairIF3Pump", "PentairIF3PumpProgram",
+          "PentairSaltLevelSensor", "PentairSumpPumpBatteryBackup" ]
 __version__ = "0.0.1"
